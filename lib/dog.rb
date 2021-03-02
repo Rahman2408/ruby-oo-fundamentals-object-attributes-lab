4b7ca6377f645a742c00ec817af5d29578bf9cf5
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-03-02 13:07:15 -0500

    commit tree f648688f52c29fa0954e33a1bec2c0217170aa1f

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+
+    def initialize_name(name)
+    @name
+    end
+  
+    def name=(name)
+    @name=name
+    end
+ 
+    def initialize_breed(breed)
+        @breed
+    end
+
+    def breed=(breed)
+        @breed=breed
+        end
+
+    def name
+      @name
+    end
+
+    def name=(new_name)
+        @name= new_name
+    end
+
+    def breed
+        @breed
+        end
+    
+    def breed=(new_breed)
+        @breed= new_breed
+    end
+
+end
